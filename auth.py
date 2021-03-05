5d51c8fe1ba799963fd56906d7becd5bdb7f2b94
IcarusCodes <stoian.alex94@yahoo.com>
2021-03-05 22:05:36 +0200

    Starting the deploy process

---
+++ auth.py
@@ -0,0 +1,72 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+accepted_users = ['Crows0', 'Crows1', 'Crows2', 'Crows3', 'Crows4', 'Crows5', 'Crows6']
+
+
+@auth.route('/login')
+def login():
+    if request.method == 'GET':
+        stage = request.args.get('stage')
+        if stage is not None:
+            if int(stage) not in range(0, 8):
+                stage = 0
+        else:
+            stage = 0
+    return render_template('login.html', stage=stage)
+
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login_post():
+
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash(f'Invalid username or password.')
+        return redirect(url_for(f'auth.login'))
+
+    if user.username.lower() not in [f'crows{x}' for x in range(0, 8)]:
+        flash(f'Only Crows users allowed.')
+        return redirect(url_for(f'auth.login', stage=stage))
+
+    login_user(user)
+
+    return redirect(url_for(f'main.crows', i=user.username[-1], msg=0))
+
+
+@auth.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        flash("User already exists")
+        return redirect(url_for('auth.signup'))
+
+    new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
---
+++ main.py
@@ -0,0 +1,68 @@
+import random, re
+
+from flask import Blueprint, render_template, request, flash, redirect, url_for, send_from_directory
+from flask_login import login_required, current_user
+from . import db
+from datetime import datetime
+
+main = Blueprint('main', __name__)
+
+
+@main.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
+
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+# @main.route('/static/crows2/files')
+# def files():
+#     # return "<h1> UGH </h1>"
+#     return send_from_directory("users.txt", "/static/crows2/files")
+
+
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
+
+
+@main.route('/crows<i>')
+@login_required
+def crows(i=""):
+    msg = request.args.get('msg')
+    if msg is None:
+        msg = "0"
+
+    msg = re.sub("[^\d]+", '', msg)
+
+    if int(i) in range(0, 8):
+        level = i
+    else:
+        level = 0
+
+    if isinstance(msg, str):
+        if msg == "":
+            msg = 0
+        if int(msg) == 0:
+            flash(f"CAAWW!! You successfully logged in at Crows{i}, good luck.", )
+            return redirect(url_for(f'main.crows', i=i, msg=1))
+        else:
+            random_message = [
+                "Did you really think crows would hide hints like this?",
+                "You do know that us crows can hold grudges right?",
+                "Crow Gang CAW-CAW ",
+                "HINT: No hints are hidden here.",
+                "CAA'CAAAWWW!!!",
+                "GIVE ME BREAD AND SHINY THINGS!",
+                "Why do you keep refreshing this page?",
+                "Those white crows are far inferior than us black crows.",
+                "The funeral for lil Jimmy the Crow has been cancelled due to heavy storms.",
+            ]
+
+            flash(random_message[random.randint(0, 8)])
+
+    return render_template(f'crows/crows{level}.html', level=f'Crows{i}')
