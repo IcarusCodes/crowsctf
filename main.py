34f24c026cc85b360a5605db2f63851989bb2977
IcarusCodes <stoian.alex94@yahoo.com>
2021-03-15 19:35:07 +0200

    Still working on fix - server side debug

--- main.py
+++ main.py
@@ -64,6 +64,8 @@ def viewsource():
 def crows7submit():
     if request.method == 'POST':
         if request.form.get('needle') != "":
+            print("Rulez comanda:")
+            print(f"grep -i {request.form.get('needle')} /tmp/words.txt")
             results = os.popen(f"grep -i {request.form.get('needle')} /tmp/words.txt").read().split("\n")
             print(f"RESULTS SUNT {results}")
             return render_template('crows/crows7.html', level='Crows7', results=results[:5])
